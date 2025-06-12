075ab07ee436826094638e4524b0985105c0da9b
rizkydnz <rizkydwinugroho88@gmail.com>
2025-06-13 03:07:52 +0700

    Klinik

--- Klinik2/src/resources/views/partials/navbar.blade.php
+++ Klinik2/src/resources/views/partials/navbar.blade.php
@@ -1,4 +1,3 @@
-
 <!-- Navbar Start -->
 <div class="container-fluid fixed-top px-0 wow fadeIn" data-wow-delay="0.1s">
     <div class="top-bar text-white-50 row gx-0 align-items-center d-none d-lg-flex">
@@ -10,7 +9,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
         <a href="{{ url('/') }}" class="navbar-brand ms-4 ms-lg-0">
-            <h1 class="fw-bold text-primary m-0">Chari<span class="text-white">Team</span></h1>
+            <h1 class="fw-bold text-primary m-0">Klinik<span class="text-white">Bersama</span></h1>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="navbar-toggler-icon"></span>
@@ -19,12 +18,12 @@
             <div class="navbar-nav ms-auto p-4 p-lg-0">
                 <a href="{{ url('/') }}" class="nav-item nav-link {{ Request::is('/') ? 'active' : '' }}">Home</a>
                 <a href="{{ url('/about') }}" class="nav-item nav-link {{ Request::is('about') ? 'active' : '' }}">About</a>
-                <a href="{{ url('/causes') }}" class="nav-item nav-link {{ Request::is('causes') ? 'active' : '' }}">Causes</a>
+                <a href="{{ url('/appointment') }}" class="nav-item nav-link {{ Request::is('appointment') ? 'active' : '' }}">Appointment</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle {{ Request::is('service') || Request::is('donate') || Request::is('team') || Request::is('testimonial') || Request::is('404') ? 'active' : '' }}" data-bs-toggle="dropdown">Pages</a>
                     <div class="dropdown-menu m-0">
                         <a href="{{ url('/service') }}" class="dropdown-item {{ Request::is('service') ? 'active' : '' }}">Service</a>
-                        <a href="{{ url('/donate') }}" class="dropdown-item {{ Request::is('donate') ? 'active' : '' }}">Donate</a>
+                        <a href="{{ url('/feature') }}" class="dropdown-item {{ Request::is('feature') ? 'active' : '' }}">Feature</a>
                         <a href="{{ url('/team') }}" class="dropdown-item {{ Request::is('team') ? 'active' : '' }}">Our Team</a>
                         <a href="{{ url('/testimonial') }}" class="dropdown-item {{ Request::is('testimonial') ? 'active' : '' }}">Testimonial</a>
                         <a href="{{ url('/404') }}" class="dropdown-item {{ Request::is('404') ? 'active' : '' }}">404 Page</a>
@@ -33,8 +32,8 @@
                 <a href="{{ url('/contact') }}" class="nav-item nav-link {{ Request::is('contact') ? 'active' : '' }}">Contact</a>
             </div>
             <div class="d-none d-lg-flex ms-2">
-                <a class="btn btn-outline-primary py-2 px-3" href="{{ url('/donate') }}">
-                    Donate Now
+                <a class="btn btn-outline-primary py-2 px-3" href="{{ url('/appointment') }}">
+                    Appointment
                     <div class="d-inline-flex btn-sm-square bg-white text-primary rounded-circle ms-2">
                         <i class="fa fa-arrow-right"></i>
                     </div>
